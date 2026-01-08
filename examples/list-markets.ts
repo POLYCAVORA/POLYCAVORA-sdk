7ec24abe98a8eb0f9a3a50670b7e94abb1664240
POLYNOCTON <polyemperion@gmail.com>
2026-01-08 13:45:11 +0700

    list-markets.ts

--- examples/list-markets.ts
+++ examples/list-markets.ts
@@ -1,7 +1,7 @@
-import { PolyEmperionSDK } from "../src/client.js";
+import { POLYNOCTONSDK } from "../src/client.js";
 
 async function main() {
-  const sdk = new PolyEmperionSDK({ debug: true });
+  const sdk = new POLYNOCTONSDK({ debug: true });
   const markets = await sdk.getMarkets();
   console.log("Total markets:", markets.length);
   console.log("First market:", markets[0]);
